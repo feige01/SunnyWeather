77c4e2f4ae330315c4e3a76bd9b34f2a793ace31
feige <feige@qq.com>
2020-06-29 13:47:22 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceFragment.kt
@@ -13,6 +13,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.sunnyweather.android.MainActivity
 import com.sunnyweather.android.R
 import com.sunnyweather.android.logic.model.Place
 import com.sunnyweather.android.ui.weather.WeatherActivity
@@ -32,7 +33,7 @@ class PlaceFragment :Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        if(viewModel.isPlaceSaved()){
+        if(viewModel.isPlaceSaved()&&activity is MainActivity){
             val place=viewModel.getSavedPlace()
             val intent=Intent(context,WeatherActivity::class.java).apply {
                 putExtra("location_lng",place.location.lng)
